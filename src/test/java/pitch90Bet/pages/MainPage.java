ba009f43b2a7288b259354fc22a973a276b9bbc9
pochikaevams <m.pochikaeva@cft.ru>
2020-07-30 11:06:15 +0700

    Тесты на главную страницу

--- src/test/java/pitch90Bet/pages/MainPage.java
+++ src/test/java/pitch90Bet/pages/MainPage.java
@@ -56,6 +56,69 @@ public class MainPage extends TestBase {
     @FindBy(xpath = "//*[@id=\"root\"]/div/div/div/header/div[2]/div[2]/div/a[1]")
     WebElement todaysPage;
 
+    @FindBy(xpath = "//*[@id=\"root\"]/div/div/main/div/div[2]/nav/div[1]/a[3]")
+    WebElement todaysCategory;
+
+    @FindBy(xpath = "//*[@id=\"root\"]/div/div/main/div/div[2]/div/div/li[1]/a/span[2]")
+    WebElement firstElementTodaysCategory;
+
+    @FindBy(xpath = "//*[@id=\"root\"]/div/div/div/header/div[2]/div[2]/div/a[2]")
+    WebElement livePage;
+
+    @FindBy(xpath = "//*[@id=\"root\"]/div/div/div/header/div[2]/div[2]/div/a[3]")
+    WebElement sportPage;
+
+    @FindBy(xpath = "//*[@id=\"root\"]/div/div/div/header/div[2]/div[2]/div/a[4]")
+    WebElement virtualPage;
+
+    @FindBy(xpath = "//*[@id=\"root\"]/div/div/main/div/div[2]/div/a[1]/img")
+    WebElement firstItemInVirtual;
+
+    @FindBy(xpath = "//*[@id=\"root\"]/div/div/div/header/div[2]/div[2]/div/a[5]")
+    WebElement helpInBar;
+
+    @FindBy(xpath = "//*[@id=\"root\"]/div/div/main/div/div[2]/div/article/p[1]/span")
+    WebElement textOnHelpPage;
+
+    @FindBy(xpath = "//*[@id=\"root\"]/div/div/div/header/div[2]/div[2]/div/a[6]")
+    WebElement aboutUsInBar;
+
+    @FindBy(xpath = "//*[@id=\"root\"]/div/div/main/div/div[2]/article/div[2]/h1")
+    WebElement textOnAboutUsPage;
+
+    @FindBy(xpath = "//*[@id=\"root\"]/div/div/div/header/div[2]/div[3]/div/div[3]")
+    WebElement in1ClickSwitchBox;
+
+    @FindBy(xpath = "//*[@id=\"root\"]/div/div/main/div/div[2]/footer/div/div[1]/nav/ul/li[1]/a")
+    WebElement todaysInFooter;
+
+    @FindBy(xpath = "//*[@id=\"root\"]/div/div/main/div/div[2]/footer/div/div[1]/nav/ul/li[2]/a")
+    WebElement liveInFooter;
+
+    @FindBy(xpath = "//*[@id=\"root\"]/div/div/main/div/div[2]/footer/div/div[1]/nav/ul/li[3]/a")
+    WebElement sportInFooter;
+
+    @FindBy(xpath = "//*[@id=\"root\"]/div/div/main/div/div[2]/footer/div/div[1]/nav/ul/li[4]/a")
+    WebElement virtualInFooter;
+
+    @FindBy(xpath = "//*[@id=\"root\"]/div/div/main/div/div[2]/footer/div/div[1]/nav/ul/li[5]/a")
+    WebElement helpInFooter;
+
+    @FindBy(xpath = "//*[@id=\"root\"]/div/div/main/div/div[2]/footer/div/div[1]/nav/ul/li[6]/a")
+    WebElement aboutUsInFooter;
+
+    @FindBy(xpath = "//*[@id=\"root\"]/div/div/main/div/div[2]/footer/div/div[1]/nav/ul/li[7]/a")
+    WebElement RGSInFooter;
+
+    @FindBy(xpath = "//*[@id=\"root\"]/div/div/main/div/div[2]/footer/div/div[1]/nav/ul/li[8]/a")
+    WebElement privacyPolicyInFooter;
+
+    @FindBy(xpath = "//*[@id=\"root\"]/div/div/main/div/div[2]/footer/div/div[1]/nav/ul/li[9]/a")
+    WebElement bonusRulesInFooter;
+
+    @FindBy(xpath = "//*[@id=\"root\"]/div/div/main/div/div[2]/footer/div/div[1]/nav/ul/li[10]/a")
+    WebElement termsInFooter;
+
     public MainPage(WebDriver driver, WebDriverWait wait) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
@@ -111,6 +174,10 @@ public class MainPage extends TestBase {
         liveCategory.click();
     }
 
+    public void clickOnTodays(){
+        livePage.click();
+    }
+
     public void clickFirstItemInSportCategory() {
         wait.visibilityOf(firstItemInSportCategory).click();
         wait.visibilityOf(firstSubItemInSportCategory).click();
@@ -120,11 +187,92 @@ public class MainPage extends TestBase {
         wait.visibilityOf(firstItemInLiveCategory).click();
     }
 
+    public void clickOnLiveCategoryInBar() {
+        wait.visibilityOf(livePage).click();
+    }
+
+    public void clickOnSportCategoryInBar() {
+        wait.visibilityOf(sportPage).click();
+    }
+
+    public void clickOnVirtualInBar() {
+        wait.visibilityOf(virtualPage).click();
+    }
+
+    public void clickOnHelpInBar() {
+        wait.visibilityOf(helpInBar).click();
+    }
+
+    public void clickOnAboutUsInBar() {
+        wait.visibilityOf(aboutUsInBar).click();
+    }
+
     public void checkSoccerSportOpened() {
         Assert.assertTrue(wait.visibilityOf(headTextOfSportEvent).getText().contains("SOCCER"));
     }
 
+    public void checkVirtualOpened() {
+        Assert.assertTrue(wait.visibilityOf(firstItemInVirtual).isDisplayed());
+    }
+
+    public void checkHelpPageOpened() {
+        Assert.assertTrue(wait.visibilityOf(textOnHelpPage).getText().contains("Terms and Conditions"));
+    }
+
+    public void checkAboutUsPageOpened() {
+        Assert.assertTrue(wait.visibilityOf(textOnAboutUsPage).getText().contains("Need Help, Clarity, Assistance? Contact Us."));
+    }
+
     public void checkLiveHeaderOpened() {
         Assert.assertTrue(wait.visibilityOf(headTextOfLiveEvent).isDisplayed());
+    }
+
+    public void checkTodaysOpened() {
+        Assert.assertTrue(wait.visibilityOf(todaysCategory).isDisplayed());
+        Assert.assertTrue(wait.visibilityOf(firstElementTodaysCategory).isDisplayed());
+    }
+
+    public void clickOn1ClickSwitchBox(){
+        in1ClickSwitchBox.click();
+    }
+
+    public void clickOnTodaysInFooter() {
+        wait.visibilityOf(todaysInFooter).click();
+    }
+
+    public void clickOnLiveInFooter() {
+        wait.visibilityOf(liveInFooter).click();
+    }
+
+    public void clickOnSportInFooter() {
+        wait.visibilityOf(sportInFooter).click();
+    }
+
+    public void clickOnVirtualInFooter() {
+        wait.visibilityOf(virtualInFooter).click();
+    }
+
+    public void clickOnHelpInFooter() {
+        wait.visibilityOf(helpInFooter).click();
+    }
+
+    public void clickOnAboutUsInFooter() {
+        wait.visibilityOf(aboutUsInFooter).click();
+    }
+
+    public void clickOnRGSInFooter() {
+        wait.visibilityOf(RGSInFooter).click();
+    }
+
+    public void clickOnPrivacyPolicyInFooter() {
+        wait.visibilityOf(privacyPolicyInFooter).click();
+    }
+
+    public void clickOnBonusRulesInFooter() {
+        wait.visibilityOf(bonusRulesInFooter).click();
+    }
+
+    public void clickOnTermsAndConditionsInFooter() {
+        wait.visibilityOf(termsInFooter).click();
     }
 }
--- src/test/java/pitch90Bet/tests/MainPageTests.java
+++ src/test/java/pitch90Bet/tests/MainPageTests.java
@@ -37,4 +37,69 @@ public class MainPageTests extends TestBase {
         mainPage.checkLiveHeaderOpened();
     }
 
+    @Test
+    public void openTodaysCategory() {
+
+        driver.get("http://localhost:8000/");
+
+        mainPage.clickOnTodays();
+        mainPage.checkTodaysOpened();
+    }
+
+    @Test
+    public void openLiveFromBar() {
+
+        driver.get("http://localhost:8000/");
+
+        mainPage.clickOnLiveCategoryInBar();
+        mainPage.clickFirstItemInLiveCategory();
+        mainPage.checkLiveHeaderOpened();
+    }
+
+    @Test
+    public void openSportFromBar() {
+
+        driver.get("http://localhost:8000/");
+
+        mainPage.clickOnLiveCategoryInBar();
+        mainPage.clickOnSportCategoryInBar();
+        mainPage.clickFirstItemInSportCategory();
+        mainPage.checkSoccerSportOpened();
+    }
+
+    @Test
+    public void openVirtualFromBar() {
+
+        driver.get("http://localhost:8000/");
+
+        mainPage.clickOnVirtualInBar();
+        mainPage.checkVirtualOpened();
+    }
+
+    @Test
+    public void openHelpFromBar() {
+
+        driver.get("http://localhost:8000/");
+
+        mainPage.clickOnHelpInBar();
+        mainPage.checkHelpPageOpened();
+    }
+
+    @Test
+    public void openAboutUsFromBar() {
+
+        driver.get("http://localhost:8000/");
+
+        mainPage.clickOnAboutUsInBar();
+        mainPage.checkAboutUsPageOpened();
+    }
+
+    @Test
+    public void setIn1ClickSwitchBox() {
+
+        driver.get("http://localhost:8000/");
+
+        mainPage.clickOn1ClickSwitchBox();
+    }
+
 }
